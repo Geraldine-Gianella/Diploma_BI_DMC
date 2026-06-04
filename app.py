9f9cea476e94a59b8a7e87f3dd2d78010b22609c
Geraldine-Gianella <119369402+Geraldine-Gianella@users.noreply.github.com>
2026-06-03 20:07:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,16 @@ st.sidebar.image("DMC.png", width=100)
 st.write("Elaborado por: Geraldine Geronimo")
 
 archivo = st.file_uploader("Cargue el archivo excel o csv")
+
+if archivo is not None:
+  if arcivo.name.endswith(".csv"):
+    data = pd.read_csv(archivo)
+    str.write(data)
+  elif archivo.name.enswith(".xlsx"):
+    data = pd.read_excel(archivo)
+    str.write(data)
+  else: 
+    str.write("Formato no válido")
+  
+else:
+  st.write("Por favor cargue su archivo")
