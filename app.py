77728eec315fc86abc1f50a8752131b387efdfb7
Geraldine-Gianella <119369402+Geraldine-Gianella@users.noreply.github.com>
2026-06-14 12:45:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -67,6 +67,8 @@ elif modulos == "Carga y perfil del dataset":
     
     if archivo.name.endswith(".csv") or archivo.name.endswith(".xlsx"):
       data = pd.read_csv(archivo)
+      # Estandarizar el nombre las columnas, todas a minúsculas
+      data.columns = data.columns.str.lower()
       
       # Vista previa del archivo 
       st.subheader("Vista previa del dataset")
@@ -139,6 +141,10 @@ elif modulos == "Carga y perfil del dataset":
       if columnas_seleccionadas:
         st.write("Resumen estadístico")
         st.dataframe(data[columnas_seleccionadas].describe(include="all"))
+
+  elif modulos == "Procesamiento de datos":
+
+    # Estandariaar el nombre de las columanas de la data 
 
   else:
     st.write("Por favor cargue su archivo")
