7c33020549e055712ad4f6d2e1aca4c25e6855e6
Geraldine-Gianella <119369402+Geraldine-Gianella@users.noreply.github.com>
2026-06-14 22:34:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # CONFIGURACIÓN DE SESSION STATE
 # Guardamos el dataset cargado
